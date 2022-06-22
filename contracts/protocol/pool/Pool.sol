24f67aba474e3061c66488a1d3c2135827ccde2a
lovecoding <lovecodingdev@gmail.com>
2022-06-23 05:40:03 +0800

    added IPool

--- contracts/protocol/pool/Pool.sol
+++ contracts/protocol/pool/Pool.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.10;
 
+import {IPool} from '../../interfaces/IPool.sol';
 import {PoolStorage} from './PoolStorage.sol';
 
-contract Pool is PoolStorage{
+contract Pool is PoolStorage, IPool{
 
 }
