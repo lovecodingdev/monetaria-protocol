01c276d9e14b1c0eabb2cfdc3daa6e2402f37183
lovecoding <lovecodingdev@gmail.com>
2023-02-09 01:01:22 +0800

    add typechain

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require('hardhat-contract-sizer');
 require("@nomiclabs/hardhat-etherscan");
+require("@typechain/hardhat");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
