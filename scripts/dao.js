bf0c2424680f718a575584bde020268f239c0103
Love Coding <lovecodingdev@gmail.com>
2023-03-17 12:50:23 -0400

    update dao script

--- scripts/dao.js
+++ scripts/dao.js
@@ -1,49 +1,7 @@
 const hre = require("hardhat");
 const { BigNumber, utils } = require('ethers');
 const { ethers } = require("hardhat");
-
-async function verify(params) {
-  try {
-    await hre.run("verify:verify", params);
-  } catch (error) {
-    if(String(error).includes("already verified")){
-      console.log(`${params.address} is already verified.`);
-    }else{
-      console.log(error)
-    }
-  }
-}
-const AVG_BLOCK_TIME = 15_000;
-
-let DEPLOYED = {};
-
-async function deploy(contractName, {config, params = [], wait = AVG_BLOCK_TIME, key} = {}) {
-  var ContractFactory;
-  if (config){
-    ContractFactory = await hre.ethers.getContractFactory(contractName, config);
-  }else{
-    ContractFactory = await hre.ethers.getContractFactory(contractName);
-  }
-  let contract;
-  let deployedKey = key || contractName;
-  if(DEPLOYED[deployedKey]){
-    contract = await ContractFactory.attach(DEPLOYED[deployedKey]);
-    console.log(`${deployedKey} already deployed to: ${contract.address}`);
-  }else{
-    contract = await ContractFactory.deploy(...params);
-    await contract.deployed();
-    await contract.deployTransaction.wait();
-    // await sleep(wait);
-    DEPLOYED[deployedKey] = contract.address;
-    console.log(`${deployedKey} deployed to: ${contract.address}`);
-    await verify({
-      address: contract.address,
-      constructorArguments: params
-    });
-    console.log(`${deployedKey} verified`);
-  }
-  return contract;
-}
+const { ZERO_ADDRESS, deploy } = require("./helpers")
 
 async function main() {
   const [signer] = await ethers.getSigners();
